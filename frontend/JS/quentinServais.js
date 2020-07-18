8c7045c172ef6329e479e0b36ea2cb4e0ab4acf5
Quentins <quentin.f.servais@student.uclouvain.be>
2020-07-18 20:04:11 +0200

    js + index

--- frontend/JS/quentinServais.js
+++ frontend/JS/quentinServais.js
@@ -2,42 +2,186 @@ let cpu = [
     {
   marque: "Intel",
   model: "I7 10700k",
-  prix: "550",
-  code: 1
+  socket: "1200",
+  fréquence: 5.1,
+  prix: 550,
+  points: 9,
+  image: "IMG/10700k.jpg"
 },
     {
     marque: "Intel",
     model: "I5 10600k",
-    prix: "330",
-    code: 1
+    socket: "1200",
+    fréquence: 4.8,
+    prix: 330,
+    points: 7,
+    image: "IMG/10600k.jpg"
 },
     {
       marque: "AMD",
       model: "R7 3700X",
-      prix: "350",
-      code: 2
+      socket: "AM4",
+      fréquence: 4.4,
+      prix: 350,
+      points: 8,
+      image: "IMG/3700x.jpg"
+},
+{
+  marque: "AMD",
+  model: "Threadripper 3990X",
+  socket: "TRX40",
+  fréquence: 4.3,
+  prix: 4700,
+  points: 10,
+  image: "IMG/3990x.jpg"
 }
 ];
-let carteMere = [{
+let carteMere = [
+  {
   marque: "MSI",
-  chipset: "Z390",
-  prix: "250",
-  code: 1
+  model: "Z490",
+  socket: "1200",
+  format: "ATX",
+  prix: 180,
+  code: 1,
+  image: "IMG/z490.jpg"
+},
+  {
+  marque: "ASRock",
+  model: "B550",
+  socket: "AM4",
+  format: "Micro-ATX",
+  prix: 170,
+  code: 2,
+  image: "IMG/b550.jpg"
+},
+  {
+  marque: "Asus",
+  model: "X470",
+  socket: "AM4",
+  format: "ATX",
+  prix: 220,
+  code: 2,
+  image: "IMG/x470.jpg"
+},
+  {
+  marque: "Gigabyte",
+  model: "TRX40",
+  socket: "TRX40",
+  format: "ATX",
+  prix: 480,
+  code: 3,
+  image: "IMG/trx40.jpg"
+},
+  {
+  marque: "MSI",
+  model: "B460",
+  socket: "1200",
+  format: "Micro-ATX",
+  prix: 80,
+  code: 1,
+  image: "IMG/b460.png"
+}
+];
+let rAM = [
+  {
+    marque: "Corsair",
+    model: "Vengeance",
+    type: "DDR4",
+    fréquence: 3600,
+    taille: 16,
+    prix: 170,
+    points: 8,
+    id: "r1",
+    image: "IMG/r1.jpg"
+  },
+  {
+    marque: "GSkill",
+    model: "Trident Z",
+    type: "DDR4",
+    fréquence: 4000,
+    taille: 32,
+    prix: 310,
+    points: 10,
+    id: "r2",
+    image: "IMG/r2.jpg"
+  },
+  {
+    marque: "Crucial",
+    model: "Ballistix",
+    type: "DDR4",
+    fréquence: 3000,
+    taille: 16,
+    prix: 130,
+    points: 7,
+    id: "r3",
+    image: "IMG/r3.jpg"
+  },
+  {
+    marque: "Corsair",
+    model: "Vengeance",
+    type: "DDR4",
+    fréquence: 3000,
+    taille: 8,
+    prix: 70,
+    points: 6,
+    id: "r4",
+    image: "IMG/r1.jpg"
+  },
+  {
+    marque: "HyperX",
+    model: "Fury",
+    type: "DDR3",
+    fréquence: 1600,
+    taille: 8,
+    prix: 40,
+    points: 4,
+    id: "r5",
+    image: "IMG/r5.jpg"
   }
 ];
+let cpuChoisis;
+let cMChoisie;
+let rAMChoisie;
+
+function selectCpu(){
+  cpuChoisis = document.getElementById("selectCpu").value;
+  for(let i = 0; i < cpu.length; i++){
+    if(cpuChoisis == cpu[i].model){
+      document.getElementById("cpuBox").innerHTML = "Vous avez choisi : " + cpu[i].marque + " " + cpu[i].model + " " + cpu[i].prix + "€";
+      cpuChoisis = cpu[i];
+      document.getElementById("cpuIm").src= cpuChoisis.image;
+    }
+  }
+}
+
+function selectCM(){
+  cMChoisie = document.getElementById("selectCM").value;
+  for(let i = 0; i < carteMere.length; i++){
+    if(cMChoisie == carteMere[i].model){
+      document.getElementById("cMBox").innerHTML = "Vous avez choisi : " + carteMere[i].marque + " " + carteMere[i].model + " " + carteMere[i].prix + "€";
+      cMChoisie = carteMere[i];
+      document.getElementById("cMIm").src= cMChoisie.image;
+    }
+  }
+}
+
+function selectRAM(){
+  rAMChoisie = document.getElementById("selectRAM").value;
+  for(let i = 0; i < rAM.length; i++){
+    if(rAMChoisie == rAM[i].id){
+      document.getElementById("rAMBox").innerHTML = "Vous avez choisi : " + rAM[i].marque + " " + rAM[i].model + " " +  rAM[i].type + " " + rAM[i].fréquence + "MHz" + " " + rAM[i].taille + "Go " + rAM[i].prix + "€";
+      rAMChoisie = rAM[i];
+      document.getElementById("rAMIm").src= rAMChoisie.image;
+    }
+  }
+}
+
 function compatibilite(){
-  if(cpu[2].code == carteMere[0].code){
+  if(cpuChoisis.socket == cMChoisie.socket){
       document.getElementById("rep").innerHTML = "C'est compatible !";
   }
   else{
     document.getElementById("rep").innerHTML = "Attention Ce n'est pas compatible !";
-  }
-}
-function selectCpu(){
-  let selection = document.getElementById("selectCpu").value;
-  for(let i = 0; i < cpu.length; i++){
-    if(selection == cpu[i].model){
-      document.getElementById("cpuBox").innerHTML = "Vous avez choisi : " + cpu[i].marque + " " + cpu[i].model + " " + cpu[i].prix + "€";
-    }
   }
 }
